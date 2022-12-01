7572559e0492de82774c7b2e18607b086b82448f
mnovaiss <marcos.novais@gmail.com>
2022-12-01 18:33:13 +0000

    Projeto finalizado

--- app.py
+++ app.py
@@ -1,27 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request, session, flash, url_for, redirect, abort
+import sqlite3
+
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
 app = Flask("Olá mundo")
 
-@app.route("/") # Rota Principal
-def hello():
-    return "Olá mundo"
+app.config.from_object(__name__)
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request 
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
+
+@app.route("/")
+def exibir_posts():
+    sql  = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC" 
+    resultado = g.bd.execute(sql)
+    posts = []
+
+    for titulo, texto, data_criacao in resultado.fetchall():
+        posts.append({
+            "titulo": titulo,
+            "texto": texto,
+            "data_criacao": data_criacao
+        })
+    return render_template("exibir_posts.html", post = posts)
 
 
-@app.route("/nome") # Rota nome
-def nome():
-    return ("Marcos Vinicius")
+@app.route("/inserir", methods =["POST", "GET"])    
+def inserir():
+    if not session.get('logado'):
+        abort(401)
+    titulo = request.form.get('titulo')
+    texto = request.form.get('texto')
+    sql = "INSERT INTO posts (titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [titulo, texto])
+    g.bd.commit()
+    flash("Novo post inserido")
+    return redirect(url_for('exibir_posts'))
 
 
-@app.route("/sobrenome") # Rota sobrenome
-def sobrenome():
-    return ("Novais Santos")
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    erro = None
+    if(request.method == "POST"):
+        if request.form['username'] == "Ocean" and request.form['password'] == "ocean1234":
+            session['logado'] = True
+            flash("Usuario logado " + request.form['username'])
+            return redirect(url_for('exibir_posts'))
+        erro = "Usuário ou senha incorretos"    
+    return render_template("login.html", erro=erro)    
 
+@app.route("/logout")
+def logout():
+    session.pop('logado', None)    
 
-@app.route("/alunas") # Rota alunas
-def alunas():
-    return render_template("alunas.html")
-
-
-@app.route("/alunos") # Rota alunos
-def alunos():
-    return render_template("hello.html")
+    flash("Logout Efetuado")
+    return redirect(url_for('exibir_posts'))
